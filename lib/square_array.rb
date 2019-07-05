8a976025678c93325e5b39430fc93115aa178e95
Luke Menard <lukemenard@gmail.com>
2019-07-05 19:07:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   while counter < array.length do
     new_numbers = array.push(array[counter]**2)
-    puts new_numbers
     counter += 1
   end
+  new_numbers
 end
